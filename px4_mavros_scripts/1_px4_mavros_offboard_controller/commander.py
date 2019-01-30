f4556d5b4e3f58d1fc3e0b53fe3ba7ac1c41d02f
doodleshr <haoransong1106@gmail.com>
2019-01-30 11:46:41 +0800

    first commit, added obstacle map, px4 mavros scripts and ygz slam with ros

---
+++ px4_mavros_scripts/1_px4_mavros_offboard_controller/commander.py
@@ -0,0 +1,33 @@
+import rospy
+from mavros_msgs.msg import GlobalPositionTarget, State
+from mavros_msgs.srv import CommandBool,SetMode
+from geometry_msgs.msg import PoseStamped, Twist
+from sensor_msgs.msg import Imu, NavSatFix
+from std_msgs.msg import Float32, String
+from pyquaternion import Quaternion
+import time
+import math
+
+
+class Commander:
+    def __init__(self):
+        rospy.init_node("commander_node")
+        rate = rospy.Rate(20)
+        self.position_target_pub = rospy.Publisher('gi/set_pose/position', PoseStamped, queue_size=10)
+        self.yaw_target_pub = rospy.Publisher('gi/set_pose/orientation', Float32, queue_size=10)
+
+
+    def move(self, x, y, z):
+        self.position_target_pub.publish(set_pose(x, y, z))
+
+    def turn(self, yaw_degree):
+        self.yaw_target_pub.publish(yaw_degree)
+
+
+    def set_pose(self, x=0, y=0, z=2):
+        pose = PoseStamped()
+        pose.header.stamp = rospy.Time.now()
+        pose.pose.position.x = x
+        pose.pose.position.y = y
+        pose.pose.position.z = z
+        return pose
