c2df35f24374b93695990c79ecd4871a263b37cd
Lambda <lambda@giai.tech>
2019-02-15 16:07:28 +0800

    added visualization tools;fixed memory error

--- algorithms/loop_closing/DBoW2/demo/demo.cpp
+++ algorithms/loop_closing/DBoW2/demo/demo.cpp
@@ -49,17 +49,23 @@ void wait()
 
 int main()
 {
+ try{
   vector<vector<cv::Mat > > features;
 
   vector<vector<cv::KeyPoint> > kp_list;
   vector<cv::Mat> kdesp_list;
   loadFeatures(features,kp_list,kdesp_list);  //image to features.
 
-  //testVocCreation(features); //Do not create any voc.Use a pretrained one.
+  testVocCreation(features); //Do not create any voc.Use a pretrained one.
 
   //wait();
 
   testDatabase(features,"small_voc.yml.gz",kp_list,kdesp_list);//do test.
+ }
+ catch (...) {
+    // this executes if f() throws std::string or int or any other unrelated type
+   cout<<"Caught error!"<<endl;
+ }
 
   return 0;
 }
@@ -156,35 +162,42 @@ void testVocCreation(const vector<vector<cv::Mat > > &features)
   cout << "Done" << endl;
 }
 
+
+
+void saveImagePair(int id1,int id2,int &save_index,std::vector<DMatch>& good_matches,vector<vector<cv::KeyPoint> > &kp_list,vector<cv::Mat> &kdesp_list);
+
 // ----------------------------------------------------------------------------
-bool match_2_images_flann(int index1,int index2,vector<vector<cv::KeyPoint> > &kp_list,vector<cv::Mat> &kdesp_list)
+
+bool match_2_images_flann(int index1,int index2,vector<vector<cv::KeyPoint> > &kp_list,vector<cv::Mat> &kdesp_list,int &save_index)
 {
   using namespace cv::xfeatures2d;
   FlannBasedMatcher matcher = FlannBasedMatcher(makePtr<flann::LshIndexParams>(12,20,2));
   std::vector< DMatch > matches;
   matcher.match( kdesp_list[index1], kdesp_list[index2], matches );
 
+  cout<<"Matched!"<<endl;
 
 
   double max_dist = 0; double min_dist = 100;
-  for( int i = 0; i < kdesp_list[index1].rows; i++ )
+  for( int i = 0; i < matches.size(); i++ )
   {
     double dist = matches[i].distance;
     if( dist < min_dist ) min_dist = dist;
     if( dist > max_dist ) max_dist = dist;
   }
+
   std::vector< DMatch > good_matches;
 
-  for( int i = 0; i < kdesp_list[index1].rows; i++ )
+  for( int i = 0; i < matches.size(); i++ )
   {
-    if( matches[i].distance <= 3*min_dist )
+    if( matches[i].distance <= 2*min_dist )
     {
       good_matches.push_back( matches[i]); 
     }
   }
 
-  std::vector<Point2f> match_points1;
-  std::vector<Point2f> match_points2;
+  std::vector<cv::Point2f> match_points1;
+  std::vector<cv::Point2f> match_points2;
 
   cout<<"Good matches count:"<<good_matches.size()<<"."<<endl;
   if(good_matches.size()<8)
@@ -195,15 +208,53 @@ bool match_2_images_flann(int index1,int index2,vector<vector<cv::KeyPoint> > &k
   for( size_t i = 0; i < good_matches.size(); i++ )
   {
     //-- Get the keypoints from the good matches
+    //cout<<"Step1_"<<i<<endl;
+    //cout<<"kp_list size:"<<kp_list.size()<<endl;
+    //cout<<"kplist index:"<<index1<<endl;
+    //auto a = kp_list[index1];
+    //cout<<"2";
+    //auto b = good_matches[i];
+    //cout <<"3";
+    //auto c = good_matches[i].queryIdx;
+    //cout<<4;
     match_points1.push_back( kp_list[index1][ good_matches[i].queryIdx ].pt );
+    //cout<<"Step2_"<<i<<endl;
+
+    //auto d = kp_list[index2];
+    //cout<<5;
+    //auto e = good_matches[i].trainIdx;
+    //cout<<6;
     match_points2.push_back( kp_list[index2][ good_matches[i].trainIdx ].pt );
   }
-
-
+  saveImagePair(index1,index2,save_index,good_matches,kp_list,kdesp_list);
   Mat fundamental_matrix = findFundamentalMat(match_points1, match_points2, FM_RANSAC, 3, 0.99);
   cout<<"MATCH SUCCESS."<<endl;
   return true;
 
+}
+
+void saveImagePair(int id1,int id2,int &save_index,std::vector<DMatch>& good_matches,vector<vector<cv::KeyPoint> > &kp_list,vector<cv::Mat> &kdesp_list)
+{
+    stringstream ss;
+    ss << "images/image" << id1 << ".png"; 
+    cv::Mat image = cv::imread(ss.str(), 0);
+    
+    stringstream ss2;
+    ss2<< "images/image"<<id2<<".png";
+    cv::Mat image2 = cv::imread(ss2.str(),0);
+
+//
+    cv::Mat merged_img;
+//    cv::hconcat(image,image2,merged_img);
+
+//use cv::drawMatches replace hconcat.
+    
+    cv::drawMatches(image,kp_list[id1],image2,kp_list[id2],good_matches,merged_img);
+    stringstream output_ss;
+
+    output_ss<<"loops/image"<<save_index<<".png";
+    cv::imwrite(output_ss.str(),merged_img);
+    save_index++;
 }
 
 
@@ -224,6 +275,7 @@ void testDatabase(const vector<vector<cv::Mat > > &features,const std::string db
 
 
   db.add(features[0]);
+  int loop_id = 0;
   for(int i = 0; i < NIMAGES; i++)
   {
   
@@ -243,15 +295,14 @@ void testDatabase(const vector<vector<cv::Mat > > &features,const std::string db
 
     cout << "Searching for Image " << i << ". " << ret << endl;
     //ret[0].Score;ret[0].Id
-    if (ret[0].Score>0.7)
+    if (ret[0].Score>0.65)
     {
       if (ret[0].Id<i-10)
       {
-        if (match_2_images_flann(i,ret[0].Id,kp_list,kdesp_list)) // check if this loop candidate satisfies Epipolar Geometry constrain.
+        if (match_2_images_flann(i,ret[0].Id,kp_list,kdesp_list,loop_id)) // check if this loop candidate satisfies Epipolar Geometry constrain.
         {
           cout<<"Loop between ["<<i<<"\t"<<ret[0].Id<<"]"<<endl;
         }
-        
       }
     }
     else
