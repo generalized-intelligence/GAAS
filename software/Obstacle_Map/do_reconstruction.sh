4523c117efcf44b64250d9154665c6b18d285288
doodleshr <haoransong1106@gmail.com>
2019-04-09 14:10:15 +0800

    Gazebo model bug fixes

--- software/Obstacle_Map/do_reconstruction.sh
+++ software/Obstacle_Map/do_reconstruction.sh
@@ -1,2 +1,2 @@
 echo "RUN DENSE_RECONSTRUCTION"
-./bin/dense_reconstruction  -l /multisense_sl/camera/right/image_raw -r /multisense_sl/camera/left/image_raw -c calibration/mynteye_sim2.yml -w 752 -h 480 -u 752 -v 480
+./bin/dense_reconstruction  -l /gi/simulation/left/image_raw -r /gi/simulation/right/image_raw -c calibration/mynteye_sim2.yml -w 752 -h 480 -u 752 -v 480
