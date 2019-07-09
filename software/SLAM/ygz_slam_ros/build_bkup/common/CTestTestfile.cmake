f7b725ea6bd0d3a13ec339bf9766b5aed59980d1
hddgi <dongshuo@giai.tech>
2019-07-09 17:41:10 +0800

    Update tutorial 6

--- software/SLAM/ygz_slam_ros/build_bkup/common/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gaas_folder/GAAS/software/SLAM/ygz_slam_ros/common
-# Build directory: /home/gaas_folder/GAAS/software/SLAM/ygz_slam_ros/build/common
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- software/SLAM/ygz_slam_ros/build_bkup/system/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gaas_folder/GAAS/software/SLAM/ygz_slam_ros/system
-# Build directory: /home/gaas_folder/GAAS/software/SLAM/ygz_slam_ros/build/system
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
