bacc21047e200de20d454137b398d9e455f208ab
Laurent Wang <lambda@makehave.com>
2021-04-11 17:29:46 +0800

    [feature] added target_point_navigator and astar_planner_node.

--- algorithms/src/Navigation/global_map_planner/src/AStarPlannerNode.cpp
+++ algorithms/src/Navigation/global_map_planner/src/AStarPlannerNode.cpp
@@ -1,151 +1,116 @@
 #include "AStarLib.h"
 #include "../../obstacle_drivable_block_publisher/src/MapBlock.h"
 
+#include <iostream>
+#include <mutex>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/persistence.hpp>
+
+#include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+
+#include <glog/logging.h>
+
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+
 #include <glog/logging.h>
 #include "Timer.h"
 
 #include "gaas_msgs/GAASNavigationPath.h"
-//test astarlib by loading a map and do navigation.
-//class AStarTestEnv
-//{
-//public:
-//    MapCloudT::Ptr pmap_cloud = nullptr;
-//    MapBlock::Ptr pMb;
-//    std::shared_ptr<ros::NodeHandle> pNH = nullptr;
-//    AStarCostMap ascm;
-//    int path_id;
-//    std::shared_ptr<ros::Publisher> pMapPub,pPathPub;
+#include "gaas_msgs/GAASGetAStarPath.h"
 
-//    void init(int argc,char** argv)
-//    {
-//        ros::init(argc,argv,"astar_debug_node");
-//        pNH = std::shared_ptr<ros::NodeHandle>(new ros::NodeHandle);
-//        pMapPub = std::shared_ptr<ros::Publisher>(new ros::Publisher);
-//        *pMapPub = pNH->advertise<visualization_msgs::MarkerArray>("/gaas/navigation/offline_map_block_generator",1);
-//        pPathPub = std::shared_ptr<ros::Publisher>(new ros::Publisher);
-//        *pPathPub = pNH->advertise<visualization_msgs::Marker>("/gaas/navigation/offline_astar_visualization",1);
-//        string map_path;
-//        if(!ros::param::get("map_path",map_path))
-//        {
-//            LOG(ERROR)<<"Error:Map path not set!"<<endl;
-//            throw "Error!";
-//        }
-//        pmap_cloud = MapCloudT::Ptr(new MapCloudT);
-//        pcl::io::loadPCDFile(map_path,*pmap_cloud);
-//        if(pmap_cloud->size() == 0)
-//        {
-//            LOG(ERROR)<<"Error:map empty!"<<endl;
-//            throw "Error!";
-//        }
-//        pMb = MapBlock::Ptr(new MapBlock);
-//        pMb->initMapBlock(pmap_cloud);
-//        ScopeTimer t("Generate AStar Map");
-//        LOG(INFO)<<"generating a* map..."<<endl;
-//        ascm.initAStarMapByBlockMap(this->pMb);
-//    }
-//    void visualizeMap()
-//    {
-//        visualization_msgs::MarkerArray mks;
-//        pMb->visualizeToMarkerArray(mks);
-//        int i = 0;
-//        for(auto& u:mks.markers)
-//        {
-//            u.id = i++;
-//        }
-//        LOG(INFO)<<"publishing marker array with "<<mks.markers.size()<<" markers!"<<endl;
-//        pMapPub->publish(mks);
-//    }
-//    vector<TIndex> testAstar(int xi,int yi,int zi,int xf,int yf,int zf)
-//    {
-//        ScopeTimer t("Run A*");
-//        LOG(INFO)<<"Test A*..."<<endl;
-//        auto u = ascm.doAstar(xi,yi,zi,xf,yf,zf);
-//        LOG(INFO)<<"u.size"<<u.size()<<",path:"<<endl;
-//        for(auto v:u)
-//        {
-//            LOG(INFO)<<v[0]<<","<<v[1]<<","<<v[2]<<endl;
-//        }
-//        return u;
-//    }
-//    void visualizeAStar(vector<TIndex>& astar_path)
-//    {
-//        visualization_msgs::Marker lineList;
-//        //lineList.header.stamp = ros::Time::now();
-//        lineList.header.frame_id = "map";
-//        lineList.type = lineList.LINE_STRIP;
-//        lineList.ns = "visualize_astar_path";
-//        lineList.action = lineList.ADD;
-//        lineList.id = this->path_id++;
-//        lineList.color.r = 1.0;
-//        lineList.color.g = 1.0;
-//        lineList.color.b = 0.0;
-//        lineList.color.a = 1.0;
-//        lineList.scale.x = 0.3;
-//        //        for(int i=0;i<astar_path.size()-1;i++)
-//        //        {
-//        //            for(int j = 0;j<2;j++)
-//        //            {
-//        //                geometry_msgs::Point pt;
-//        //                auto index = astar_path.at(i+j);
-//        //                int x=index[0],y=index[1],z=index[2];
-//        //                pt.x = ascm.original_map->blockAt(x,y,z).cx;
-//        //                pt.y = ascm.original_map->blockAt(x,y,z).cy;
-//        //                pt.z = ascm.original_map->blockAt(x,y,z).cz;
-//        //                lineList.points.push_back(pt);
-//        //            }
-//        //        }
-//        lineList.colors.resize(astar_path.size());
-//        for(int i=0;i<astar_path.size();i++)
-//        {
-//            geometry_msgs::Point pt;
-//            auto index = astar_path.at(i);
-//            int x=index[0],y=index[1],z=index[2];
-//            pt.x = ascm.original_map->blockAt(x,y,z).cx;
-//            pt.y = ascm.original_map->blockAt(x,y,z).cy;
-//            pt.z = ascm.original_map->blockAt(x,y,z).cz;
-//            lineList.colors.at(i).r=1.0;
-//            lineList.colors.at(i).g=1.0;
-//            lineList.colors.at(i).b=0.0;
-//            lineList.colors.at(i).a=1.0;
-//            lineList.points.push_back(pt);
-//        }
-//        lineList.pose.orientation.w = 1;
-//        pPathPub->publish(lineList);
-//        LOG(INFO)<<"Visualizer published astar path!"<<endl;
-//        sleep(4);
-//    }
-//};
+class AStarPlannerNode
+{
+public:
+    MapCloudT::Ptr pmap_cloud = nullptr;
+    MapBlock::Ptr pMb;
+    std::shared_ptr<ros::NodeHandle> pNH = nullptr;
+    AStarCostMap ascm;
+    int path_id;
+    ros::ServiceServer service_server;
+    //std::shared_ptr<ros::Publisher> pMapPub,pPathPub;
+    void initAStarPlannerNode(int argc,char** argv)
+    {
+        ros::init(argc,argv,"astar_planner_node");
+        pNH = std::shared_ptr<ros::NodeHandle>(new ros::NodeHandle);
+        //pMapPub = std::shared_ptr<ros::Publisher>(new ros::Publisher);
+        //*pMapPub = pNH->advertise<visualization_msgs::MarkerArray>("/gaas/navigation/offline_map_block_generator",1);
+        //pPathPub = std::shared_ptr<ros::Publisher>(new ros::Publisher);
+        //*pPathPub = pNH->advertise<visualization_msgs::Marker>("/gaas/navigation/offline_astar_visualization",1);
+        string map_path;
+        if(!ros::param::get("map_path",map_path))
+        {
+            LOG(ERROR)<<"Error:Map path not set!"<<endl;
+            throw "Error!";
+        }
+        pmap_cloud = MapCloudT::Ptr(new MapCloudT);
+        pcl::io::loadPCDFile(map_path,*pmap_cloud);
+        if(pmap_cloud->size() == 0)
+        {
+            LOG(ERROR)<<"Error:map empty!"<<endl;
+            throw "Error!";
+        }
+        pMb = MapBlock::Ptr(new MapBlock);
+        pMb->initMapBlock(pmap_cloud);
+        ScopeTimer t("Generate AStar Map");
+        LOG(INFO)<<"generating a* map..."<<endl;
+        ascm.initAStarMapByBlockMap(this->pMb);
+        this->service_server = this->pNH->advertiseService("/gaas/navigation/global_planner/astar_planning",&AStarPlannerNode::planningServiceCallback,this);
+        ros::spin();
+    }
+    bool planningServiceCallback(gaas_msgs::GAASGetAStarPath::Request& request,gaas_msgs::GAASGetAStarPath::Response& response)
+    {
+        if(request.header.frame_id!="map")
+        {
+            LOG(ERROR)<<"ERROR: in planningServiceCallback():Request not in map coordinate!"<<endl;
+            return false;
+        }
+        int ix,iy,iz,fx,fy,fz;//initial and final xyz.
+        auto& i_ = request.current_point.point;
+        auto& f_ = request.target_point.point;
 
+        ascm.original_map->getMapBlockIndexByXYZ(i_.x,i_.y,i_.z,ix,iy,iz);//map的初始位置
+        ascm.original_map->getMapBlockIndexByXYZ(f_.x,f_.y,f_.z,fx,fy,fz);//map的目标
+
+        vector<TIndex> output_path = ascm.doAstar(ix,iy,iz,fx,fy,fz);
+        if(output_path.size()==0)
+        {
+            response.success=false;
+        }
+        else
+        {
+            response.success=true;
+            response.path.header.frame_id = "map";
+            response.path.header.stamp = ros::Time::now();
+            response.path.path_nodes.clear();
+            for(auto n:output_path)
+            {
+                geometry_msgs::Point pt;
+                pt.x = n[0];
+                pt.y = n[1];
+                pt.z = n[2];
+                response.path.path_nodes.push_back(pt);
+            }
+        }
+        return true;
+    }
+};
+ bool planningServiceCallback222(gaas_msgs::GAASGetAStarPath::Request& request,gaas_msgs::GAASGetAStarPath::Response& response)
+ {
+     return true;
+ }
 int main(int argc,char** argv)
 {
     FLAGS_alsologtostderr = 1;
     google::InitGoogleLogging("astar_debug_node");
-    gaas_msgs::GAASNavigationPath path;
-    //AStarTestEnv aenv;
-//    aenv.init(argc,argv);
-//    aenv.visualizeMap();
-
-//    int ix,iy,iz,fx,fy,fz;
-
-//    //test 1.
-//    aenv.ascm.original_map->getMapBlockIndexByXYZ(0,0,0,ix,iy,iz);
-//    aenv.ascm.original_map->getMapBlockIndexByXYZ(0,-30,0,fx,fy,fz);
-//    auto path = aenv.testAstar(ix,iy,iz,fx,fy,fz);
-//    aenv.visualizeAStar(path);
-
-//    aenv.ascm.original_map->getMapBlockIndexByXYZ(35,15,0,fx,fy,fz);
-//    path = aenv.testAstar(ix,iy,iz,fx,fy,fz);
-//    aenv.visualizeAStar(path);
-
-////    aenv.visualizeAStar(path);
-////    path = aenv.testAstar(0,0,5,0,5,7);
-////    aenv.visualizeAStar(path);
-////    path = aenv.testAstar(0,0,5,0,5,7);//test same path;
-////    path = aenv.testAstar(0,0,5,0,58,7);
-////    aenv.visualizeAStar(path);
-//    path = aenv.testAstar(0,0,5,2500,3,500);//test out of range condition.
-//    LOG(INFO)<<"test finished."<<endl;
-//    //aenv.visualizeMap();
-
+    AStarPlannerNode node;
+    node.initAStarPlannerNode(argc,argv);
     return 0;
 }
---
+++ algorithms/src/Navigation/target_point_navigator/src/target_point_navigator_node.cpp
@@ -0,0 +1,9 @@
+
+int main(int argc,char** argv)
+{
+    //step<1> get target position in map coordinate.
+    //while(!reached_target)
+    //    step<2> call a* planning module srv for new path to target.
+    //    step<3> publish new local goal.
+    return 0;
+}
