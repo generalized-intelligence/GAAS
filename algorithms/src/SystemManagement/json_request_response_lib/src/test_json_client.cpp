bd6782973d9bb1c35de3d03e35a930acff9846a7
Lambda <lambda@makehave.com>
2021-08-06 22:33:07 +0800

    [feature] added json_http_request_module.

--- algorithms/src/SystemManagement/json_request_response_lib/src/test_json_client.cpp
+++ algorithms/src/SystemManagement/json_request_response_lib/src/test_json_client.cpp
@@ -1,26 +1,49 @@
 #include "third_party/cpp-httplib/httplib.h"
-
+#include "EncryptedJSON.h"
+#include <sstream>
 
 using std::cout;
 using std::endl;
 
 
-int do_request()
-{
-    httplib::Client cli("localhost", 8080);
-    if (auto res = cli.Get("/hi"))
-    {
-        cout << res->status << endl;
-        cout << res->get_header_value("Content-Type") << endl;
-        cout << res->body << endl;
-    } else {
-        cout << "error code: " << res.error() << endl;
-    }
-    return 0;
-}
+//int do_request()
+//{
+//    httplib::Client cli("localhost", 8080);
+//    if (auto res = cli.Get("/hi"))
+//    {
+//        cout << res->status << endl;
+//        cout << res->get_header_value("Content-Type") << endl;
+//        cout << res->body << endl;
+//    } else {
+//        cout << "error code: " << res.error() << endl;
+//    }
+//    return 0;
+//}
 
 int main(int argc,char** argv)
 {
-    do_request();
+    for(int i = 0;i<100;i++)
+    {
+        EncryptedJSON::EncryptedJSONRequest json_req;
+        json j;
+        std::stringstream ss("{\"function\": \"query\"}");
+        ss>>j;
+        json_req.start_request(j);
+        EncryptedJSON::EncryptedJSONResponse::Ptr pRes;
+        while(!json_req.getResponseSafe(pRes)&&!json_req.everTimeout())
+        {
+            cout<<"still waiting..."<<endl;
+            usleep(20000);
+        }
+        if(json_req.everTimeout())
+        {
+            cout<<"Error:timeout!"<<endl;
+        }
+        cout<<"Result:"<<pRes->pResponse_json->dump(4)<<endl;
+        if(pRes->reason_of_failure==std::string("http_error"))
+        {
+            cout<<"Request has failed!"<<endl;
+        }
+    }
     return 0;
 }
--- algorithms/src/SystemManagement/json_request_response_lib/src/test_json_server.cpp
+++ algorithms/src/SystemManagement/json_request_response_lib/src/test_json_server.cpp
@@ -13,6 +13,10 @@ int start_server()
     svr.Get("/hi", [](const httplib::Request& req, httplib::Response& res) {
         res.set_content("Hello World!", "text/plain");
     });
+    svr.Post("/api/fetchJSONObjectResponse",[](const httplib::Request& req, httplib::Response& res){
+        res.set_content("{\"state\":\"correct\"}", "text/plain");
+    });
+    cout<<"Server start..."<<endl;
     svr.listen("localhost", 8080);
     return 0;
 }
